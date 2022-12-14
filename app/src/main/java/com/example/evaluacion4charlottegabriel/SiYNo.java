a0cada53d0c49184f2dee2d66dcd7c07e640f572
Charlotte Rodriguez <euphoria.head@gmail.com>
2022-12-14 20:05:46 -0300

    Codigo de las cartas si o no quizas

--- app/src/main/java/com/example/evaluacion4charlottegabriel/SiYNo.java
+++ app/src/main/java/com/example/evaluacion4charlottegabriel/SiYNo.java
@@ -23,15 +23,32 @@ public class SiYNo extends AppCompatActivity {
         tvresultado = findViewById(R.id.Siynoresultado);
         ivcarta = findViewById(R.id.Siynoimagencarta);
     }
-    private void SiNoQuizas(){
-        int numero = (int)(Math.random()*78);
-        int[] si = {0,1,3,6,7,8,10,14,17,19,20,21,22,23,24,25,27,29,30,32,33,34,35,36,37,38,41,44,45,46,47,48,49,50,60,61,64,66,69,71,72,73,74,75,76,77};
-        int[] no = {};
-        int[] quizas = {};
 
-
-
-
+    private void SiNoQuizas() {
+        int numero = (int) (Math.random() * 78);
+        int[] si = {
+                0, 1, 3, 6, 7,
+                8, 10, 14, 17, 19,
+                20, 21, 22, 23, 24,
+                25, 27, 29, 30, 32,
+                33, 34, 35, 36, 37,
+                38, 41, 44, 45, 46,
+                47, 48, 49, 50, 60,
+                61, 64, 66, 69, 71,
+                72, 73, 74, 75, 76,
+                77
+        };
+        int[] no = {
+                13, 15, 16, 18, 26,
+                28, 31, 40, 43, 52,
+                54, 56, 57, 58, 59,
+                67, 68
+        };
+        int[] quizas = {
+                2, 4, 5, 9, 11,
+                12, 39, 42, 51, 53,
+                55, 62, 63, 65, 70
+        };
 
     }
 }
