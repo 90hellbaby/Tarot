2145c556f9456b7a39eef4569831618f5a559405
Charlotte Rodriguez <euphoria.head@gmail.com>
2022-12-14 19:39:24 -0300

    cartitas incluidas

--- app/src/main/java/com/example/evaluacion4charlottegabriel/MainActivity.java
+++ app/src/main/java/com/example/evaluacion4charlottegabriel/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.evaluacion4charlottegabriel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public  void iniciar_actividad_siyno(View view){
+        Intent i = new Intent( this, SiYNo.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/evaluacion4charlottegabriel/SiYNo.java
+++ app/src/main/java/com/example/evaluacion4charlottegabriel/SiYNo.java
@@ -2,13 +2,36 @@ package com.example.evaluacion4charlottegabriel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SiYNo extends AppCompatActivity {
+    private TextView tvtitulo;
+    private TextView tvdescripcion;
+    private TextView tvresultado;
+    private ImageView ivcarta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_si_yno);
+        tvtitulo = findViewById(R.id.Siynotitulocarta);
+        tvdescripcion = findViewById(R.id.Siynodescripcion);
+        tvresultado = findViewById(R.id.Siynoresultado);
+        ivcarta = findViewById(R.id.Siynoimagencarta);
+    }
+    private void SiNoQuizas(){
+        int numero = (int)(Math.random()*78);
+        int[] si = {0,1,3,6,7,8,10,14,17,19,20,21,22,23,24,25,27,29,30,32,33,34,35,36,37,38,41,44,45,46,47,48,49,50,60,61,64,66,69,71,72,73,74,75,76,77};
+        int[] no = {};
+        int[] quizas = {};
+
+
+
+
+
     }
 }
