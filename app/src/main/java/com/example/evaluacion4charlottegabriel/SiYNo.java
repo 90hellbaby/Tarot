2d36db62b11200c9d7d5b05f1feb27de64aa1911
Charlotte Rodriguez <euphoria.head@gmail.com>
2022-12-14 21:07:53 -0300

    serotonina añadicion de icono y foto de perfil lectura de si y no

--- app/src/main/java/com/example/evaluacion4charlottegabriel/SiYNo.java
+++ app/src/main/java/com/example/evaluacion4charlottegabriel/SiYNo.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Arrays;
+
 public class SiYNo extends AppCompatActivity {
     private TextView tvtitulo;
     private TextView tvdescripcion;
@@ -22,11 +24,12 @@ public class SiYNo extends AppCompatActivity {
         tvdescripcion = findViewById(R.id.Siynodescripcion);
         tvresultado = findViewById(R.id.Siynoresultado);
         ivcarta = findViewById(R.id.Siynoimagencarta);
+        SiNoQuizas();
     }
 
     private void SiNoQuizas() {
         int numero = (int) (Math.random() * 78);
-        int[] si = {
+        Integer[] si = {
                 0, 1, 3, 6, 7,
                 8, 10, 14, 17, 19,
                 20, 21, 22, 23, 24,
@@ -38,17 +41,26 @@ public class SiYNo extends AppCompatActivity {
                 72, 73, 74, 75, 76,
                 77
         };
-        int[] no = {
+        Integer[] no = {
                 13, 15, 16, 18, 26,
                 28, 31, 40, 43, 52,
                 54, 56, 57, 58, 59,
                 67, 68
         };
-        int[] quizas = {
+        Integer[] quizas = {
                 2, 4, 5, 9, 11,
                 12, 39, 42, 51, 53,
                 55, 62, 63, 65, 70
         };
+        if (Arrays.asList(si).contains(numero)){
+            tvresultado.setText("Tu respuesta es si₍ᐢ. ̫ .ᐢ₎");
+        }
+        else if (Arrays.asList(no).contains(numero)){
+            tvresultado.setText("Tu respuesta es no lo siento(ó﹏ò｡)");
+        }
+        else {
+            tvresultado.setText("(ง ˃ ³ ˂)ว ⁼³₌₃⁼³ Tu respuesta es talves");
 
+        }
     }
 }
