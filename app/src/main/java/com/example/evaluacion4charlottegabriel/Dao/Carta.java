24fa24ca45bd03eb98ac03e1a04cac9d8649bc11
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-12-18 15:57:59 -0300

    Cambio de Carta para que incluya todas las descripciones

--- app/src/main/java/com/example/evaluacion4charlottegabriel/Dao/Carta.java
+++ app/src/main/java/com/example/evaluacion4charlottegabriel/Dao/Carta.java
@@ -3,10 +3,17 @@ package com.example.evaluacion4charlottegabriel.Dao;
 import java.io.Serializable;
 
 public class Carta implements Serializable {
-
     private String titulo;
+    private String descripcion;
+    private String descripcionInvertida;
+    private String descripcionAmorosa;
 
-    public void setTitulo(String titulo) {
+    public Carta() {
+
+    }
+
+    public Carta(String titulo, String descripcion) {
+        this.descripcion = descripcion;
         this.titulo = titulo;
     }
 
@@ -14,22 +21,31 @@ public class Carta implements Serializable {
         return titulo;
     }
 
-
-    private String descripcion;
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
     public String getDescripcion() {
         return descripcion;
     }
 
-    public Carta() {
-
-    }
-    public Carta(String titulo, String descripcion){
+    public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
-        this.titulo = titulo;
+    }
+
+    public String getDescripcionInvertida() {
+        return descripcionInvertida;
+    }
+
+    public void setDescripcionInvertida(String descripcionInvertida) {
+        this.descripcionInvertida = descripcionInvertida;
+    }
+
+    public String getDescripcionAmorosa() {
+        return descripcionAmorosa;
+    }
+
+    public void setDescripcionAmorosa(String descripcionAmorosa) {
+        this.descripcionAmorosa = descripcionAmorosa;
     }
 }
